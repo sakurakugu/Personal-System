4c8f2974e7bba75691692c270b3db2fa82ca33c9
sakurakugu <171350650@qq.com>
2026-05-09 23:08:17 +0800

    删除: 移除 `image-classifier` 目录及其所有文件

--- apps/desktop/python/image-classifier/src/backends/__init__.py
+++
@@ -1,5 +0,0 @@
-from .mock import MockClassifierBackend
-from .ollama import OllamaBackend
-from .openai_compatible import OpenAICompatibleBackend
-
-__all__ = ["MockClassifierBackend", "OllamaBackend", "OpenAICompatibleBackend"]
